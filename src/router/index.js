36c954ea477fd465631f3c3cfae0b6840e436277
helingna <helingna@youzan.com>
2018-01-22 15:33:48 +0800

    改回去回来的状态，原因：props方式最好用于子组件显示父组件中的数据信息，而不适用于子组件修改父组件的信息

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+const aComponent = resolve => require(['../pages/a.vue'], resolve)
+const bComponent = resolve => require(['../pages/b.vue'], resolve)
+
+
+Vue.use(VueRouter)
+
+//使用AMD方式加载
+const routes = [
+    {
+        path: '/a',
+        component: aComponent,
+        props: true
+    },{
+        path: '/b',
+        component: bComponent,
+        props: true
+    },
+
+    ]
+
+const router = new VueRouter({
+    routes,
+    mode: 'hash', //default: hash ,history
+})
+
+
+
+
+export default router
